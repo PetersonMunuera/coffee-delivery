b1c7d3082565480ce4d2da6f7b1e519aafb994c8
Peterson Munuera <peterson.munuera@gmail.com>
2024-05-22 18:51:02 -0300

    feat: header navigation links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,24 @@
 import { HeaderContainer, HeaderContent } from './styles'
 import logoCoffee from '../../assets/logo-coffee-delivery.svg'
 import { MapPin, ShoppingCart } from '@phosphor-icons/react'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={logoCoffee} alt="" />
+        <NavLink to="/">
+          <img src={logoCoffee} alt="" />
+        </NavLink>
 
         <nav>
           <span>
             <MapPin size={22} weight="fill" />
             Porto Alegre, RS
           </span>
-          <a href="#">
+          <NavLink to="/checkout">
             <ShoppingCart size={22} weight="fill" />
-          </a>
+          </NavLink>
         </nav>
       </HeaderContent>
     </HeaderContainer>
