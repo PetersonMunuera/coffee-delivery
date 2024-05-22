f77df5b97916185e363d8879a03e2c02b00f3d55
Peterson Munuera <peterson.munuera@gmail.com>
2024-05-22 16:24:40 -0300

    feat: theme config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 function App() {
-  return <h1>Hello world</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      hello world
+    </ThemeProvider>
+  )
 }
 
 export default App
---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme.yellow};
+  }
+
+  body {
+    background-color: ${(props) => props.theme.background};
+    color: ${(props) => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, button, input, textarea {
+    font: 400 1rem 'Roboto', sans-serif;
+  }
+`
